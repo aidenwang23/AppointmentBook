c13ba1f3d39af8051ef166a96adaeece9bb34209
aidenwang23 <148901698+aidenwang23@users.noreply.github.com>
2024-11-07 16:49:44 +0000

    Complete

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -1,8 +1,10 @@
 public class AppointmentBook 
 {
-    public AppointmentBook()
-    {
+    private boolean[][] schedule;
 
+    public AppointmentBook(boolean[][] schedule)
+    {
+        this.schedule = schedule;
     }
 
     /**
@@ -12,8 +14,7 @@ public class AppointmentBook
      */
     private boolean isMinuteFree(int period, int minute) 
     {
-        /* implementation not shown */
-        return false;
+        return schedule[period-1][minute];
     }
 
     /**
@@ -24,7 +25,7 @@ public class AppointmentBook
      */
     private void reserveBlock(int period, int startMinute, int duration) 
     {
-        /* implementation not shown */ 
+        for(int i = startMinute; i < startMinute + duration; i++) schedule[period - 1][i] = false;
     }
 
     /**
@@ -37,7 +38,16 @@ public class AppointmentBook
      */
     public int findFreeBlock(int period, int duration) 
     {
-        /* to be implemented in part (a) */ 
+        int block = 0;
+        for(int i = 0; i < 60; i++)
+        {
+            if(isMinuteFree(period, i))
+            {
+                block++;
+                if (block == duration) return i - duration + 1;
+            }
+            else block = 0;
+        }
         return -1;
     }
 
@@ -51,9 +61,22 @@ public class AppointmentBook
      */
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
     {
-        /* to be implemented in part (b) */ 
+        for (int i = startPeriod; i <= endPeriod; i++)
+        {
+            int freeBlock = findFreeBlock(i, duration);
+            if (freeBlock > -1)
+            {
+                reserveBlock(i, freeBlock, duration);
+                return true;
+            }
+        }
         return false;
     }
     // There may be instance variables, constructors, and methods that are not
     // shown.
+
+    public void printPeriod(int period)
+    {
+        for(int i = 0; i < schedule[period-1].length; i++) System.out.println(i + " " + schedule[period-1][i]);
+    }
 }
